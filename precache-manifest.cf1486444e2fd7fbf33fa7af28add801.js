32cde5cc6b8b39d29a4287c1a7c6d9fc91832257
 Katia S <katesakowicz@MacBook-Pro-4.local>
2020-05-30 15:04:35 +0200

    Updates

---
+++ precache-manifest.cf1486444e2fd7fbf33fa7af28add801.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2adcedecd74852510ceae90cc5236015",
+    "url": "/simple-note-reminder/index.html"
+  },
+  {
+    "revision": "ad0e6c632447eb0f667c",
+    "url": "/simple-note-reminder/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1ba2431c171a6d42e761",
+    "url": "/simple-note-reminder/static/css/main.b048b75f.chunk.css"
+  },
+  {
+    "revision": "ad0e6c632447eb0f667c",
+    "url": "/simple-note-reminder/static/js/2.1ef94282.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/simple-note-reminder/static/js/2.1ef94282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ba2431c171a6d42e761",
+    "url": "/simple-note-reminder/static/js/main.c9291148.chunk.js"
+  },
+  {
+    "revision": "ab852d40bec1c02a0f1a",
+    "url": "/simple-note-reminder/static/js/runtime-main.064c3717.js"
+  }
+]);
